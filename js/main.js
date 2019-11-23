be39da799dc102296e513c00597338ca8e50e8f9
Bronson Brown <Brsn90@icloud.com>
2019-11-22 20:09:46 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>Hello Steve!!!!!!</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//export default main;
